4dba98a5dbb093ad23726a4fa5830a17dad73d18
HMGoose <77287972+Temujin142857@users.noreply.github.com>
2026-05-20 14:57:09 -0400

    Add new origin to CORS configuration

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://tomionagano.ca']
+  origin: ['http://localhost:5173', 'https://tomionagano.ca', 'https://temujin142857.ca']
 }));
 
 const server = http.createServer(app);
